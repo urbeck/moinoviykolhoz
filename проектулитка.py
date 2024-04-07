dbbd1814749920898fea9d6285925225678b1db8
Александр <166290396+AlecsandrTrapeznikov@users.noreply.github.com>
2024-04-07 15:26:10 +0500

    Update проектулитка.py

--- проектулитка.py
+++ проектулитка.py
@@ -5,32 +5,32 @@ fps = time.Clock()
 game = True
 while game:
     for e in event.get():
-    if e.type == QUIT:
-        game = False
-    if e.type == KEYDOWN:
-        if e.key==K_RIGHT:
-            xforce = 5
-        if e.key==K_LEFT:
-            xforce = -5
-        if e.key==K_DOWN:
-            yforce = 5
-        if e.key==K_UP:
-            yforce = -5
-      if e.type == KEYUP:
-          if e.key==K_RIGHT:
-              xforce = 0
-          if e.key==K_LEFT:
-              xforce = 0
-          if e.key==K_DOWN:
-              yforce = 0
-          if e.key==K_UP:
-              yforce = 0
-    if e.type == MOUSEBUTTONDOWN:
-        if e.button == 1:
-            mx = e.pos[0]
-            my = e.pos[1]
+        if e.type == QUIT:
+            game = False
+        if e.type == KEYDOWN:
+            if e.key==K_RIGHT:
+                xforce = 5
+            if e.key==K_LEFT:
+                xforce = -5
+            if e.key==K_DOWN:
+                yforce = 5
+            if e.key==K_UP:
+                yforce = -5
+        if e.type == KEYUP:
+              if e.key==K_RIGHT:
+                  xforce = 0
+              if e.key==K_LEFT:
+                  xforce = 0
+              if e.key==K_DOWN:
+                  yforce = 0
+              if e.key==K_UP:
+                  yforce = 0
+        if e.type == MOUSEBUTTONDOWN:
+            if e.button == 1:
+                mx = e.pos[0]
+                my = e.pos[1]
 
 
-  display.update()
+    display.update()
 
 
