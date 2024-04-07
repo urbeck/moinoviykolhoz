f9e62a9d9f59d3d9eb2f7eb6ff3375c5aa600b22
Александр <166290396+AlecsandrTrapeznikov@users.noreply.github.com>
2024-04-07 14:49:41 +0500

    Update ksafhksd.py

--- ksafhksd.py
+++ ksafhksd.py
@@ -1,17 +1,17 @@
-prent(zo-zo-zo)
+print("zo-zo-zo")
+a = 1
+int = a + 5
 
-if int = a + (5):
+if int = a + 5:
   b = 44 + a
-else is:
-  z=4
-while 1:
-  while 1:
-    print("oh...')
 else:
-  r = 5
-  z = 4
+  z=4
+while True:
+    print("oh...')
+
+r = 5
+z = 4
 h = a+b
 None = 1
 oem as z = 3
-return brak
 # :)
