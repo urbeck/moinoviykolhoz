69b414dfe139d032c5c096e7c8a76c54c8566c0f
KodGOS <166290277+KodGOS@users.noreply.github.com>
2024-04-07 15:00:30 +0500

    Update ksafhksd.py
    Крч вроде пашет

--- ksafhksd.py
+++ ksafhksd.py
@@ -1,18 +1,13 @@
 print("zo-zo-zo")
-a = 1
-int = a + 5
-
-if int = a + 5:
+a = 0
+if a + 5 == int:
   b = 44 + a
-  return b
 else:
-  z=4
+  z = 4
 while True:
-    print("oh...')
-
-r = 5
-z = 4
-h = a+b
-None = 1
-oem as z = 3
+  print("oh...")
+else:
+  r = 5
+  z = 4
+h = a + b
 # :)
