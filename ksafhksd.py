3776411b87e71c6f9b897ac60544d07811734185
urbeck <74239018+urbeck@users.noreply.github.com>
2024-04-07 14:34:24 +0500

    Update ksafhksd.py

--- ksafhksd.py
+++ ksafhksd.py
@@ -4,11 +4,14 @@ if int = a + (5):
   b = 44 + a
 else is:
   z=4
-undo while:
-  do while:
+while 1:
+  while 1:
+    print("oh...')
 else:
   r = 5
   z = 4
 h = a+b
 None = 1
 oem as z = 3
+return brak
+# :)
